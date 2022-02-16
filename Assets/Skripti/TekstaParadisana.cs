fa05b2e2a7846bb6ad845e235605d093418cc71f
Eduards <eduards.polis2@gmail.com>
2022-02-16 14:14:54 +0200

    Ieviesusi pogas un vel darbibas ar tekstugit add .

---
+++ Assets/Skripti/TekstaParadisana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaParadisana : MonoBehaviour {
+	//Uzglabas tekstu kurs panemts no input lauka
+	public string teksts;
+	//Lauks no kura es to tekstu panemsu
+	public GameObject ievadesLauks;
+	//teksta objekts kur attelot mainiga Teksta saturu
+	public GameObject tekstaAttelosanasLauks;
+
+	public void darbibasArTekstu(){
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		tekstaAttelosanasLauks.GetComponent<Text>().text = "Sveiks " + teksts.ToUpper () + "!";
+	}
+}
